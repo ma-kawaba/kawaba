1573c39ddd9f1bfc230056420d8c784a1c027881
Cosmonaut Kitten <james@maple.id.au>
2024-12-13 16:15:04 +1100

    update website to latest version of kawaba, update html, css, font, images, and favicon

--- main.js
+++
@@ -1,8 +0,0 @@
-function dropdownClick(x) {
-  document.getElementById("myDropdown").classList.toggle("show");
-  x.classList.toggle("change");
-}
-
-function getStarted() {
-  window.location.href = 'pages/Lesson1.html';
-}
