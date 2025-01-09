93aa73a5d51e43a29d8eba81ba7b297410953c86
cosmonautkitten <james@maple.id.au>
2025-01-09 19:37:23 +1100

    Update rules.js

--- parser/rules.js
+++ parser/rules.js
@@ -11,12 +11,12 @@ function build_rules() {
     // \x02 is the ASCII char:       002   2     02    STX (start of text)
     // Full sentence: includes all the `X la, Y la, ... Z`
     // Partial sentence: includes only one la/main-block
-    let WORD_SEPARATOR             = /([\x02;.·…!?“”\s])/.source;
-    let FULL_SENTENCE_SEPARATOR    = /(([\x02;.·…!?“”])\s*)/.source;
+    let WORD_SEPARATOR             = /([\x02;.·…!?“”]|\s+)/.source;
+    let FULL_SENTENCE_SEPARATOR    = /([\x02;.·…!?“”]\s*)/.source;
     let PARTIAL_SENTENCE_SEPARATOR = /([\x02;.·…!?“”:]|\bnan\b|\bnen\b|\bnin\b|\bnon\b|\bnun\b)/.source;
     let PREFIXES = 'a|e|i|o|u|an|en|in|on|un';
     let CONJUNCTIONS = 'nan|nen|nin|non|nun';
-    let PREVERBS = 'wile|sona|awen|kama|ken|lukin|open|pini|alasa';
+    let MARKERS = 'na|ne|ni|no|nu';
     let PROPER_NOUNS = "((Jan|Jen|Jon|Jun|Kan|Ken|Kin|Kon|Kun|Lan|Len|Lin|Lon|Lun|Man|Men|Min|Mon|Mun|Nan|Nen|Nin|Non|Nun|Pan|Pen|Pin|Pon|Pun|San|Sen|Sin|Son|Sun|Tan|Ten|Ton|Tun|Wan|Wen|Win|An|En|In|On|Un|Ja|Je|Jo|Ju|Ka|Ke|Ki|Ko|Ku|La|Le|Li|Lo|Lu|Ma|Me|Mi|Mo|Mu|Na|Ne|Ni|No|Nu|Pa|Pe|Pi|Po|Pu|Sa|Se|Si|So|Su|Ta|Te|To|Tu|Wa|We|Wi|A|E|I|O|U)(jan|jen|jon|jun|kan|ken|kin|kon|kun|lan|len|lin|lon|lun|man|men|min|mon|mun|nan|nen|nin|non|nun|pan|pen|pin|pon|pun|san|sen|sin|son|sun|tan|ten|ton|tun|wan|wen|win|ja|je|jo|ju|ka|ke|ki|ko|ku|la|le|li|lo|lu|ma|me|mi|mo|mu|na|ne|ni|no|nu|pa|pe|pi|po|pu|sa|se|si|so|su|ta|te|to|tu|wa|we|wi)*)";
 
     let endsWithPartialSentenceBegin = new RegExp('(' + PARTIAL_SENTENCE_SEPARATOR + ')$');
@@ -111,10 +111,16 @@ function build_rules() {
         verbNonInitial: new Err(
             [
                 new RegExp(
-                    PARTIAL_SENTENCE_SEPARATOR + '((\\s*\\w+\\s+)+)((?!' + PREFIXES + ')\\w*)'
+                    PARTIAL_SENTENCE_SEPARATOR + '((\\s*\\w+\\s+)+)((?!' + PREFIXES + ')\\w+)' + WORD_SEPARATOR
                 ),
+                function(match, behind) {
+                    if (match[5] && (CONJUNCTIONS.includes(match[5]) || MARKERS.includes(match[5]))) {
+                        return false
+                    }
+                    return true
+                }
             ],
-            'A sentence may not contain multiple verbs',
+            'A verb must appear at the beginning of a sentence',
             'error',
             null,
             (key, match) => {
@@ -132,6 +138,41 @@ function build_rules() {
                     {
                         text: match[5].replace(/\x02/g, ''),
                         ruleName: key,
+                        match: match,
+                    },
+                    {
+                        text: match[6].replace(/\x02/g, ''),
+                        ruleName: 'punctuation',
+                        match: match,
+                    },
+                ];
+            }
+        ),
+
+        multipleHyphens : new Err(
+            [
+                new RegExp(
+                    WORD_SEPARATOR + '\\s*((\\w+-\\w*){2,})' + WORD_SEPARATOR
+                ),
+            ],
+            'A word with multiple hyphens is ambiguous',
+            'nitpick',
+            null,
+            (key, match) => {
+                return [
+                    {
+                        text: match[2].replace(/\x02/g, ''),
+                        ruleName: 'punctuation',
+                        match: match,
+                    },
+                    {
+                        text: match[3].replace(/\x02/g, ''),
+                        ruleName: key,
+                        match: match,
+                    },
+                    {
+                        text: match[5].replace(/\x02/g, ''),
+                        ruleName: 'punctuation',
                         match: match,
                     },
                 ];
